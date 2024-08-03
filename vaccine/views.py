2a5d92472c418c3653ca9fbf3e812af35e0253ff
Salim Miah <salimmiah0785@gmail.com>
2024-08-03 15:45:40 -0700

    done

--- vaccine/views.py
+++ vaccine/views.py
@@ -88,10 +88,26 @@ class DoseDetailView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def patch(self, request, pk):
+        dose = self.get_object(pk, request.user)
+        serializer = DoseSerializer(dose, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+ 
     def delete(self, request, pk):
         dose = self.get_object(pk, request.user)
         dose.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
 
 class AvailableHospitalListView(APIView):
     def get(self, request):
