02a912ec61dc323d2c84ac15011b6ff017c8b84c
Salim Miah <salimmiah0785@gmail.com>
2024-08-15 00:29:37 -0700

    ok

--- immunohub/settings.py
+++ immunohub/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True 
 
-# CSRF_TRUSTED_ORIGINS = ['https://vaccination-management-zii9.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://vaccination-management-wbw3.onrender.com']
 
 
 # Application definition
