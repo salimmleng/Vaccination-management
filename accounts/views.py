ddccf6c174fd13879b19a7d8c840ac5ba6d9db9d
Salim Miah <salimmiah0785@gmail.com>
2024-08-17 12:51:03 -0700

    final

--- accounts/views.py
+++ accounts/views.py
@@ -47,7 +47,7 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect("https://salimmleng.github.io/Final-vaccine-management/login.html")
+        return redirect("https://salimmleng.github.io/vaccine-management/login.html")
     else:
         return redirect("register")
 
