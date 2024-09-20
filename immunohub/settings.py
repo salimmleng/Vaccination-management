6684c58f0e3a1fb358a527a1bd1a1431b92cba28
Salim Miah <salimmiah0785@gmail.com>
2024-09-19 18:53:41 -0700

    done

--- immunohub/settings.py
+++ immunohub/settings.py
@@ -110,21 +110,21 @@ WSGI_APPLICATION = 'immunohub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://vaccine_management_sjix_user:1oX7j6pEUkaqlHqjlv43E0BJYMRtkzLN@dpg-cquesslsvqrc73bni210-a.oregon-postgres.render.com/vaccine_management_sjix',
-        
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://vaccine_management_sjix_user:1oX7j6pEUkaqlHqjlv43E0BJYMRtkzLN@dpg-cquesslsvqrc73bni210-a.oregon-postgres.render.com/vaccine_management_sjix',
+        
+#     )
+# }
 
 
 
